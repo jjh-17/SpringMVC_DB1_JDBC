737866f0a64506854db95996e7cc4a94b3fade96
JJH <jakebbak@gmail.com>
2023-05-31 11:47:07 +0900

    Initial commit

---
+++ src/test/java/SpringMVC_DB1/JDBC/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringMVC_DB1.JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
