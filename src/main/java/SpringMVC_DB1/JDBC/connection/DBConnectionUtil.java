4b14605eaa215056a1633d6241a3fee97abb8db4
JJH <jakebbak@gmail.com>
2023-06-01 14:09:04 +0900

    2023/06/01: DriverManager를 이용한 DB 연결 메서드 정의

---
+++ src/main/java/SpringMVC_DB1/JDBC/connection/DBConnectionUtil.java
@@ -0,0 +1,25 @@
+package SpringMVC_DB1.JDBC.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static SpringMVC_DB1.JDBC.connection.ConnectionConst.*;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    public static Connection getConnection() {
+        try{
+            //연결 시도
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
