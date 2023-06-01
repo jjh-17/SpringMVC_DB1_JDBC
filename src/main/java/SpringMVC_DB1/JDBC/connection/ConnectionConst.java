4d54e537b3143ee664b331baf03a6faeaf9d870e
JJH <jakebbak@gmail.com>
2023-06-01 14:08:30 +0900

    2023/06/01: DB Connection에 사용할 상수

---
+++ src/main/java/SpringMVC_DB1/JDBC/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package SpringMVC_DB1.JDBC.connection;
+
+//데이터 베이스 접속 필요 정보 상수로 정의
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/H2데이터베이스/스프링 기초/SpringMVC_DB1/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
