3bedda3fcf068a22d42fd28f1b003f3d61ae9222
JJH <jakebbak@gmail.com>
2023-06-01 14:07:36 +0900

    2023/06/01: 멤버 객체

---
+++ src/main/java/SpringMVC_DB1/JDBC/domain/Member.java
@@ -0,0 +1,21 @@
+package SpringMVC_DB1.JDBC.domain;
+
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String memberId;
+    private int money;
+
+    public Member() {
+
+    }
+
+    public Member(String memberId, int money) {
+        this.memberId = memberId;
+        this.money = money;
+    }
+
+}
