7107cd3b60f04c8136f192883fda8de800cc29d4
jjh <jjho311@naver.com>
2024-08-04 18:17:49 +0900

    Refactor: DB 접속에 필요한 상수 환경 변수에서 가져오도록 수정

--- src/main/java/SpringMVC_DB1/JDBC/connection/ConnectionConst.java
+++ src/main/java/SpringMVC_DB1/JDBC/connection/ConnectionConst.java
@@ -1,9 +1,13 @@
 package SpringMVC_DB1.JDBC.connection;
 
-//데이터 베이스 접속 필요 정보 상수로 정의
+import io.github.cdimascio.dotenv.Dotenv;
+
+// 데이터 베이스 접속 필요 정보 상수로 정의
 public abstract class ConnectionConst {
 
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "";
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static final String URL = dotenv.get("SPRING_DATASOURCE_URL");
+    public static final String USERNAME = dotenv.get("SPRING_DATASOURCE_USERNAME");
+    public static final String PASSWORD = dotenv.get("SPRING_DATASOURCE_PASSWORD");
 }
