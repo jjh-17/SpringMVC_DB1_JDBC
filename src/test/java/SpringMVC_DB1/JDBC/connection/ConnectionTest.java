c250a7b6402df04067b41563ac8734a5fef7ea5c
jjh <jjho311@naver.com>
2024-08-05 15:20:09 +0900

    Refactor: 각 코드를 더 쉽게 비교할 수 있도록 수정

--- src/test/java/SpringMVC_DB1/JDBC/connection/ConnectionTest.java
+++ src/test/java/SpringMVC_DB1/JDBC/connection/ConnectionTest.java
@@ -13,37 +13,34 @@ import static SpringMVC_DB1.JDBC.connection.ConnectionConst.*;
 @Slf4j
 public class ConnectionTest {
 
+    // DriverManager를 통한 커넥션 획득
     @Test
     public void driverManager() throws Exception {
-        //given
+        // given
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        log.info("con1={}, class={}", con1, con1.getClass());
-        log.info("con2={}, class={}", con2, con2.getClass());
-
-        //when
 
         //then
-
+        showConnectionInfo(con1, con2);
     }
 
-    //Driver Manager 테스트
+    // Driver Manager 테스트
     @Test
     public void dataSourceDriverManager() throws Exception {
-        //DriverManaberDatasource: 항상 새로운 커넥션을 획득
+        // DriverManagerDatasource: 항상 새로운 커넥션을 획득
         //given
         DriverManagerDataSource ds = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-
-        //when
+        Connection con1 = ds.getConnection();
+        Connection con2 = ds.getConnection();
 
         //then
-        useDataSource(ds);
+        showConnectionInfo(con1, con2);
     }
 
-    //커넥션 풀 테스트
+    // 커넥션 풀 테스트
     @Test
     public void dataSourceConnectionPoolTest() throws Exception {
-        //given
+        // given
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(URL);
         dataSource.setUsername(USERNAME);
@@ -51,20 +48,16 @@ public class ConnectionTest {
         dataSource.setMaximumPoolSize(10);
         dataSource.setPoolName("test pool");
 
-        //when
-        useDataSource(dataSource);
-
-        //커넥션 생성 작엄은 별도의 스레드에서 작동 ==> 대기 시간을 주어 커넥션 생성 로그 확인 ==> 왜 안되지
-        Thread.sleep(1000);
-
-        //then
-
-    }
-
-    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
 
+        //then
+        showConnectionInfo(con1, con2);
+        Thread.sleep(1000);  // 커넥션 생성 작엄은 별도의 스레드에서 작동 ==> 대기 시간을 주어 커넥션 생성 로그 확인 ==> 왜 안되지
+    }
+
+    // 커넥션 정보 출력
+    private void showConnectionInfo(Connection con1, Connection con2) {
         log.info("con1={}, class={}", con1, con1.getClass());
         log.info("con2={}, class={}", con2, con2.getClass());
     }
