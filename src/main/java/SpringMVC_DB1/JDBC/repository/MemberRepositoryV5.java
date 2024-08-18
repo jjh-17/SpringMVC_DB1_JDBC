e24f97b9d060eef8089495d701af31af9f8d9ad4
jjh <jjho311@naver.com>
2024-08-18 18:41:42 +0900

    Feat: JdbcTemplate을 이용한 반복 코드 제거 및 테스트

---
+++ src/main/java/SpringMVC_DB1/JDBC/repository/MemberRepositoryV5.java
@@ -0,0 +1,59 @@
+package SpringMVC_DB1.JDBC.repository;
+
+import SpringMVC_DB1.JDBC.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import javax.sql.DataSource;
+
+/**
+ * JdbcTemplate 사용
+ * 반복 코드 제거
+ */
+@Slf4j
+public class MemberRepositoryV5 implements MemberRepository {
+
+    private final JdbcTemplate template;
+
+    public MemberRepositoryV5(DataSource dataSource) {
+        template = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+        template.update(sql, member.getMemberId(), member.getMoney());
+        return member;
+    }
+
+
+    //파라미터로 Connection을 받을 필요 없어짐
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+        return template.queryForObject(sql, memberRowMapper(), memberId);
+    }
+
+    //파라미터로 Connection을 받을 필요 없어짐
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+        template.update(sql, money, memberId);
+    }
+
+    //정보 삭제
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id=?";
+        template.update(sql, memberId);
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setMemberId(rs.getString("member_id"));
+            member.setMoney(rs.getInt("money"));
+            return member;
+        };
+    }
+}
